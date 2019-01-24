eccfc61c234097bf7a1094b48ca85b50e1678312
Dana Simmons <dsanalog@gmail.com>
2019-01-24 01:17:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,6 +5,9 @@ ugly_hash = {}
 data.each do |top_key, mid_hashes|
   mid_hashes.each do |mid_key, name_array|
     name_array.each do |name|
+      if !ugly_hash.has_key?(name)
+        ugly_hash[name] = {}
+      end
       if !ugly_hash[name].has_key?(top_key)
         ugly_hash[name][top_key] = *"#{mid_key}"
       else

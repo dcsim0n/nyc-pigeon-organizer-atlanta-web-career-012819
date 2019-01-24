491d9c59d493589ff0ba3fcb308361b8fc242f9f
Dana Simmons <dsanalog@gmail.com>
2019-01-24 01:05:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,11 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
 ugly_hash = {}
 
 data.each do |top_key, mid_hashes|
   mid_hashes.each do |mid_key, name_array|
     name_array.each do |name|
+      binding.pry
       if !ugly_hash[name][top_key]
         ugly_hash[name][top_key] = *mid_key
       else
